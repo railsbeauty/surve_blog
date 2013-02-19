cc38d97bff71d42628aa77a1a658d0229d4bc38f
Vinay <vinay59.grewal@gmail.com>
2013-02-20 02:20:02 +0530

    completed the article part

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,19 @@ class ArticlesController < ApplicationController
    redirect_to article_path(@article)
   end
 
+  def edit
+  	@article = Article.find(params[:id])
+  end
+  
+  def update
+   @article = Article.find(params[:id])
+
+   @article.update_attributes(params[:article])
+    flash.notice = "Article '#{@article.title}' Updated!"
+   redirect_to article_path(@article)
+  end
+
+
   def destroy 
    @article = Article.find(params[:id])
    @article.destroy
