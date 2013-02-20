a80880199fc2af13efdc98aba8bfedc823624ffd
vinay <vinay@vertisinfotech.com>
2013-02-20 16:36:45 +0530

    done

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,9 +1,8 @@
 class Tag < ActiveRecord::Base
   attr_accessible :name
   has_many :taggings
-  has_many :articles, through: :taggings
+  has_many :taggings, dependent: :destroy
  
-
   def to_s
     name
   end
