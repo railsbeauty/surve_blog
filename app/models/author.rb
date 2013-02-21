ef19ae007828ea5146bc5681f3c2948142501608
Vinay <vinay59.grewal@gmail.com>
2013-02-22 02:11:47 +0530

    added validation for email

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,4 +4,5 @@ attr_accessible :username, :email, :password, :password_confirmation
   authenticates_with_sorcery!
   attr_accessible :title, :body
   validates_confirmation_of :password, message: "should match confirmation", if: :password
+  validates_uniqueness_of :email, :case_sensitive => false
 end
